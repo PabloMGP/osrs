74d05419f673bc4ebec185106f5dcee3b1b24545
Pablo <126181041+PabloMGP@users.noreply.github.com>
2026-05-11 18:40:48 -0300

    Create general_graardor.plugin.kts

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/npcs/definitions/other/general_graardor.plugin.kts
@@ -0,0 +1,44 @@
+package gg.rsmod.plugins.content.npcs.definitions.bosses
+
+val ids = intArrayOf(6260)
+
+ids.forEach {
+    set_combat_def(it) {
+        configs {
+            attackSpeed = 6
+            respawnDelay = 90
+        }
+
+        stats {
+            hitpoints = 2550
+            attack = 280
+            strength = 350
+            defence = 250
+        }
+
+        bonuses {
+            attackStab = 0
+            attackSlash = 0
+            attackCrush = 120
+
+            defenceStab = 90
+            defenceSlash = 90
+            defenceCrush = 90
+            defenceMagic = 80
+            defenceRanged = 80
+
+            attackBonus = 100
+            strengthBonus = 120
+        }
+
+        anims {
+            attack = 7060
+            death = 7062
+            block = 7061
+        }
+
+        aggro {
+            radius = 10
+        }
+    }
+}
